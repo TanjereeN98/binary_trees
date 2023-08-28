f0a73cd63370572f8f788d89876b93ab5f4b7307
TanjereeN98 <ahmadcyc35@gmail.com>
2023-08-29 00:24:27 +0300

    Adding extra validation to is_leaf function, to make sure parent isn't null

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,7 +11,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (!node)
         return (0);
-    if (!(node->left) && !(node->right))
+    if (!(node->left) && !(node->right) && (node->parent))
         return (1);
     return (0);
 }
