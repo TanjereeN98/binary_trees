8515997a35674834866a2a32848aa1f65a8ffd96
Islam Hany <ihhg.developer@gmail.com>
2023-08-29 15:45:41 +0300

    Post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -34,5 +34,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
