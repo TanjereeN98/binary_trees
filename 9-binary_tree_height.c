aef4e663ac015ed85b1a37dbe7ae50653aec9e11
Islam Hany <ihhg.developer@gmail.com>
2023-08-29 16:08:48 +0300

    Fix Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,14 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
+
 	if (!tree || (!(tree->left) && !(tree->right)))
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
